d226e1b5baa52aec4b9688fbecf0f241537c75e0
Adarsh <adarshgupta2468@gmail.com>
2022-11-30 19:59:05 +0530

    added routes folder + userlookup

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,15 @@
 const express=require("express")
 const app=express()
 const port=3000
+const router=express().router()
 
 
 app.get("/",(req,res)=>{
     res.send("Hello World")
 })
+const user=require("./routes/user_lookup.js")
+
+app.use("/user",user)
 app.listen(port,()=>{
     console.log("Server is running on port "+port)
 })
